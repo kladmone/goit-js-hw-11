5d7aa0b66781fda8512596552f5752cd624db445
klodmone <kladmone54@gmail.com>
2023-11-11 22:33:41 +0100

    Summary

--- src/index.js
+++ src/index.js
@@ -1,19 +1,58 @@
+import Notiflix from 'notiflix';
 import { searchImages } from './usersApi.js';
 const refs = {
   formEl: document.querySelector('.search-form'),
   formBtn: document.querySelector('.btn'),
+  gallery: document.querySelector('.gallery'),
 };
-
+let query = '';
 refs.formEl.addEventListener('submit', onFormSubmit);
 
 async function onFormSubmit(event) {
   event.preventDefault();
-
+  const query = event.target.elements.searchQuery.value;
   try {
-    const searchQuery = event.currentTarget.elements.searchQuery.value;
-    const images = await searchImages(searchQuery);
-    console.log(searchQuery);
+    const images = await searchImages(query);
+    if (images.length === 0) {
+      Notiflix.Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    renderImages(images);
   } catch (error) {
-    console.error('Error:', error.message);
+    console.log(error);
   }
+}
+
+function renderTemplate({
+  webformatURL,
+  largeImageURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  return `<div class="photo-card">
+  <img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads ${downloads}</b>
+    </p>
+  </div>
+</div>`;
+}
+function renderImages(array) {
+  const markup = array.map(renderTemplate).join('');
+  refs.gallery.innerHTML = markup;
 }
--- src/usersApi.js
+++ src/usersApi.js
@@ -1,20 +1,21 @@
-const axios = require('axios');
-
+import axios from 'axios';
 export async function searchImages(query) {
   const apiKey = '40604957-a713f0ac0088deb25f7100ca0';
   const apiUrl = 'https://pixabay.com/api/';
+  const perPage = 40;
   const params = {
     key: apiKey,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+    page: page,
+    per_page: perPage,
   };
 
   try {
     const response = await axios.get(apiUrl, { params });
     const images = response.data.hits;
-    console.log(images);
     return images;
   } catch (error) {
     console.log(error);
